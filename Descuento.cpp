d9d10f1133a9d74dc249fdcca5aac631502a54cf
Nataren8 <00375426@uca.edu.sv>
2026-09-21 10:24:51 -0600

    creacion de algoritmos de descuentos y temperatura

---
+++ Descuento.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+
+float monto =0.0;
+float total = 0.0;
+
+cout<<"ingrese el monto de compras total: ";
+
+cin>>monto;
+
+if (monto>100 && monto <=200)
+{
+    total = monto * 0.90;
+    cout<<"el monto total con descuento de 10 porciento es de: " <<total<< endl;
+}
+else if( monto > 200){
+
+        total = monto * 0.80;
+        cout<<"el monto total con 20 porciento descuento es de: " <<total<< endl;
+}
+else{
+    total = monto;
+    cout<<"No hay descuento a compras menores de 100$ total: " <<total<< endl;
+}
+
+
+}
--- Temperatura.cpp
+++ Temperatura.cpp
@@ -4,6 +4,23 @@ using namespace std;
 
 int main(){
 
+float temperatura =0.0;
 
-    
+cout<<"ingrese la temperatura en c°: ";
+
+cin>>temperatura;
+
+if (temperatura <= 10)
+{
+    cout<<"La temperatura es fria"<<endl;
+}
+else if (temperatura > 10 && temperatura <=25)
+{
+    cout<<"La temperatura es templada"<<endl;   
+}
+else{
+    cout<<"La temperatura es calurosa"<<endl;
+}
+
+    return 0;
 }
